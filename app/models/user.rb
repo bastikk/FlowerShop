cc8cca90d0786caacd5d0c0a14520b0839fb80fe
Ivan Bastylo <Ivan_Bastylo@epam.com>
2021-02-19 12:55:54 +0200

    Added and passed user test+ roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
+require 'role_model'
+
 class User < ApplicationRecord
+  include RoleModel
+  after_initialize :init
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  roles :admin, :customer
+
+  def init
+    self.roles = [:customer]
+  end
 end
--- spec/features/create_products_spec.rb
+++ spec/features/create_products_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper.rb'
-#visit root route
+# visit root route
 # click create
 # fill forms
 # click submit
@@ -10,10 +10,10 @@ require 'rails_helper.rb'
 
 feature 'Work with products' do
   scenario 'Create product' do
-    #visit root route
+    # visit root route
     visit '/'
     # click create
-    click_button "Create product"
+    click_button 'Create product'
     # fill forms
     fill_in 'Title', with: 'title'
     fill_in 'Description', with: 'description'
@@ -29,37 +29,37 @@ feature 'Work with products' do
   end
 
   scenario 'Edit product' do
-    #create test object
-    Product.new(title: "title", description: "description").save
-    #withit page of product
-    visit "/products/1"
-    #click edit product
-    click_button "Edit"
-    #click back
-    click_button "Back"
+    # create test object
+    Product.new(title: 'title', description: 'description').save
+    # visit page of product
+    visit '/products/1'
+    # click edit product
+    click_button 'Edit'
+    # click back
+    click_button 'Back'
     # expect content
     expect(page).to have_content('title')
     expect(page).to have_content('description')
     # click edit
-    click_button "Edit"
-    #edit fields
+    click_button 'Edit'
+    # edit fields
     fill_in 'Title', with: 'title edited'
     fill_in 'Description', with: 'description edited'
-    #save changes
+    # save changes
     click_button 'Update Product'
-    #expect appropriate content
+    # expect appropriate content
     expect(page).to have_content('title edited')
     expect(page).to have_content('description edited')
   end
 
   scenario 'delete product' do
-    #create test object
-    Product.new(title: "title", description: "description").save
-    #withit page of product
-    visit "/products/1"
-    #click delete product
-    click_button( "Delete")
-    #expect miss of deleted post
+    # create test object
+    Product.new(title: 'title', description: 'description').save
+    # visit page of product
+    visit '/products/1'
+    # click delete product
+    click_button('Delete')
+    # expect miss of deleted post
     expect(page).not_to have_content('title')
     expect(page).not_to have_content('description')
   end
@@ -67,6 +67,56 @@ end
 
 feature 'Work with users' do
   scenario 'Check permissions' do
-    #go to post page
+    # create test object
+    Product.new(title: 'title', description: 'description').save
+    # visit root
+    visit '/'
+    # visit page of product
+    visit '/products/1'
+    # expect miss of buttons as not signed in
+    expect(page).not_to have_button('Edit')
+    expect(page).not_to have_button('Delete')
+    visit '/users/sign_up'
+    fill_in 'Email', with: 'a1a1a1@gmail.com'
+    fill_in 'Password', with: 'a1a1a1'
+    fill_in 'Password confirmation', with: 'a1a1a1'
+    # click button
+    click_button 'Sign up'
+    # visit page of product
+    visit '/products/1'
+    # expect buttons as signed in
+    expect(page).to have_button('Edit')
+    expect(page).to have_button('Delete')
+  end
+end
+
+feature 'Layout navigation' do
+  scenario 'Check necessary buttons' do
+    # visit root
+    visit '/'
+
+    #not sign in
+    # expect buttons
+    expect(page).to have_button('Edit')
+    expect(page).to have_button('Delete')
+    # expect miss of buttons
+    expect(page).not_to have_button('Edit')
+    expect(page).not_to have_button('Delete')
+
+    #visit register and create user
+    visit '/users/sign_up'
+    fill_in 'Email', with: 'a1a1a1@gmail.com'
+    fill_in 'Password', with: 'a1a1a1'
+    fill_in 'Password confirmation', with: 'a1a1a1'
+    # click button
+    click_button 'Sign up'
+
+    #not sign in
+    # expect buttons
+    expect(page).to have_button('Edit')
+    expect(page).to have_button('Delete')
+    # expect miss of buttons
+    expect(page).not_to have_button('Edit')
+    expect(page).not_to have_button('Delete')
   end
 end
