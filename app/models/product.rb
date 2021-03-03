60551f95cb819b22ae68406b98e0840499896328
Ivan Bastylo <Ivan_Bastylo@epam.com>
2021-03-03 15:07:31 +0200

    Added pictures into views

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
-  has_attached_file :product_img, style: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :product_img
   validates_attachment_content_type :product_img, content_type: /\Aimage\/.*\z/
 end
