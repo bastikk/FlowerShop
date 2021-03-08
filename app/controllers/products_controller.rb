c3b6383e0780444678188d2b25a47534e56e2510
Ivan Bastylo <Ivan_Bastylo@epam.com>
2021-03-08 16:18:18 +0200

    fixed products creation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -43,7 +43,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :product_img)
+    params.require(:product).permit(:title, :description, :category_id, :product_img)
   end
 
   def find_product
