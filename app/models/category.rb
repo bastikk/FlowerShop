2e8cc58b903f66d4ddf69d3b54086f8a92ea73ab
Ivan Bastylo <Ivan_Bastylo@epam.com>
2021-03-13 19:21:11 +0200

    Added pages and controller for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
-  # has_many :products
+  has_many :products
+
+  has_attached_file :product_img
+  validates_attachment_content_type :product_img, content_type: /\Aimage\/.*\z/
 end
