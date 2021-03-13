8160b206f84c151b7642823285f5447b2a26a3d8
AndriyBosik <andriybosik2002@ukr.net>
2021-03-13 17:22:23 +0200

    Main page design was developed

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,6 @@ ActiveRecord::Schema.define(version: 2021_03_07_190258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "delivery_types", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
